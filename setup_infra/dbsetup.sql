a6238d0740b3369c8a5a2f3d25d22fad2a27b705
DropieIon <ciubotaruion@gmail.com>
2023-04-23 10:34:32 +0300

    Realised I didn't need the escape after each line :))

--- setup_infra/dbsetup.sql
+++ setup_infra/dbsetup.sql
@@ -1,15 +1,15 @@
-CREATE TABLE LOGIN_DETAILS(\
-    ID          int NOT NULL,\
-    USERNAME    varchar(255),\
-    PASS_HASH   varchar(255),\
-    IS_MEDIC    varchar(1) DEFAULT 'N',\
-    MAIL_CHECK  varchar(1) DEFAULT 'N',\
-    PRIMARY KEY (ID)\
-); \
-CREATE TABLE MEDIC_DETAILS(\
-    ID          int NOT NULL,\
-    SNAME       varchar(255),\
-    LNAME       varchar(255),\
-    RATING      FLOAT(2,2),\
-    PRIMARY KEY (ID)\
+CREATE TABLE LOGIN_DETAILS(
+    ID          int NOT NULL,
+    USERNAME    varchar(255),
+    PASS_HASH   varchar(255),
+    IS_MEDIC    varchar(1) DEFAULT 'N',
+    MAIL_CHECK  varchar(1) DEFAULT 'N',
+    PRIMARY KEY (ID)
+);
+CREATE TABLE MEDIC_DETAILS(
+    ID          int NOT NULL,
+    SNAME       varchar(255),
+    LNAME       varchar(255),
+    RATING      FLOAT(2,2),
+    PRIMARY KEY (ID)
 );
