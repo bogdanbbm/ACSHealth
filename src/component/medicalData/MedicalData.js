e901a59e1554094a8bd5c3691b2de9bacca13d5b
ALEXANDRA ANTON <alexandra.anton@bearingpoint.com>
2023-05-08 22:08:11 +0300

    nu i bun

--- src/component/medicalData/MedicalData.js
+++ src/component/medicalData/MedicalData.js
@@ -3,16 +3,26 @@ import useToken from "../../hooks/useToken";
 import Login from "../login/Login";
 
 function MedicalData() {
-  const {token, setToken} = useToken();
+  // const {token, setToken} = useToken();
+  //
+  // if (!token) {
+  //   return <Login setToken={setToken} />
+  // }
 
-  if (!token) {
-    return <Login setToken={setToken} />
-  }
+    var personal_data = [];
+    var medical_history = [];
+
 
   return (
-    <div>
+    <div className="login-wrapper">
       <h1>Medical Data</h1>
-      <p>test</p>
+        <div>
+            <h2>Personal Data</h2>
+            <ul id="personal_data_list"></ul>
+            <h2>Medical History</h2>
+            <ul id="medical_history_list"></ul>
+        </div>
+
     </div>
   );
 }
