ff172fa601327b56b3a013e98567c456bcef938f
DropieIon <ciubotaruion@gmail.com>
2023-05-24 17:59:50 +0300

    Changed setup infra after sqlalchemy refactor (thanks Razvan)

--- setup_infra/setup_infra.sh
+++ setup_infra/setup_infra.sh
@@ -7,21 +7,4 @@ pip install flask flask_mysqldb flask_cors pyjwt flask_sqlalchemy
 
 # start containers
 docker compose up -d
-
-# wait for the containers (mysql espacially) to start
-sleep 60
-
-# setup the database
-commands=`cat dbsetup.sql`
-docker exec mysql-database bash -c "mysql -uprod -psomething_encrypt3d ip -e \"${commands}\""
-
-# check that everything works
-# docker exec mysql-database bash -c "mysql -uprod -psomething_encrypt3d ip -e \"DESC LOGIN_DETAILS;\""
-docker exec mysql-database bash -c "mysql -uprod -psomething_encrypt3d ip -e \"DESC MEDIC_DETAILS;\""
-docker exec mysql-database bash -c "mysql -uprod -psomething_encrypt3d ip -e \"DESC REVIEWS;\""
-docker exec mysql-database bash -c "mysql -uprod -psomething_encrypt3d ip -e \"DESC PATIENT_DATA;\""
-docker exec mysql-database bash -c "mysql -uprod -psomething_encrypt3d ip -e \"DESC WEIGHT_HISTORY;\""
-docker exec mysql-database bash -c "mysql -uprod -psomething_encrypt3d ip -e \"DESC CONSULTATION_LIST;\""
-docker exec mysql-database bash -c "mysql -uprod -psomething_encrypt3d ip -e \"DESC BLOOD_DONATION;\""
-
 echo "${GREEN} Enjoy!"
